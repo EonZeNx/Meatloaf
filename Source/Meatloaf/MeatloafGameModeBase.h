7cb7ea17c8bf80bd07494d10abc6bad9cf3b46d3
EonZeNx <sparck.va@gmail.com>
2021-05-30 15:02:00 +1200

    Init

---
+++ Source/Meatloaf/MeatloafGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MeatloafGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEATLOAF_API AMeatloafGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
