7591b58b90b27b3aa16b76190f453d8e9d67ac46
Terence <terenco12345@gmail.com>
2021-06-20 18:47:12 +1200

    Player inventory actor component, which represents the player's inventory

---
+++ Source/Meatloaf/Private/Inventory/ActorComponents/PlayerInventoryActorComponent.cpp
@@ -0,0 +1,81 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Inventory/ActorComponents/PlayerInventoryActorComponent.h"
+
+
+// Sets default values for this component's properties
+UPlayerInventoryActorComponent::UPlayerInventoryActorComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// Initialize inventory
+	Inventory = UBaseItemStackContainer::Make(50);
+}
+
+
+// Called when the game starts
+void UPlayerInventoryActorComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UPlayerInventoryActorComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                                   FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+FItemStack UPlayerInventoryActorComponent::GetItemStackAtIndex(const int Index) const
+{
+	return Inventory->GetItemStackAtIndex(Index);
+}
+
+void UPlayerInventoryActorComponent::SetItemStackAtIndex(const int Index, const FItemStack ItemStack)
+{
+	return Inventory->SetItemStackAtIndex(Index, ItemStack);
+}
+
+void UPlayerInventoryActorComponent::SwapItemStacks(const int FirstIndex, const int SecondIndex)
+{
+	return Inventory->SwapItemStacks(FirstIndex, SecondIndex);
+}
+
+bool UPlayerInventoryActorComponent::CanInsertItemStack_Implementation(const FItemStack ItemStack) const
+{
+	return Inventory->CanInsertItemStack(ItemStack);
+}
+
+FItemStack UPlayerInventoryActorComponent::InsertItemStack_Implementation(const FItemStack ItemStack)
+{
+	return Inventory->InsertItemStack(ItemStack);
+}
+
+bool UPlayerInventoryActorComponent::CanRemoveItemStack_Implementation(const FItemStack ItemStack) const
+{
+	return Inventory->CanRemoveItemStack(ItemStack);
+}
+
+FItemStack UPlayerInventoryActorComponent::RemoveItemStack_Implementation(const FItemStack ItemStack)
+{
+	return Inventory->RemoveItemStack(ItemStack);
+}
+
+TArray<FItemStack> UPlayerInventoryActorComponent::GetAllItemStacks_Implementation() const
+{
+	return Inventory->GetAllItemStacks();
+}
+
+TArray<FItemStack> UPlayerInventoryActorComponent::RemoveAllItemStacks_Implementation()
+{
+	return Inventory->RemoveAllItemStacks();
+}
---
+++ Source/Meatloaf/Public/Inventory/ActorComponents/PlayerInventoryActorComponent.h
@@ -0,0 +1,75 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Interfaces/InventoryActorComponentInterface.h"
+#include "Inventory/Models/BaseItemStackContainer.h"
+#include "Inventory/Models/Structs/ItemStack.h"
+
+#include "PlayerInventoryActorComponent.generated.h"
+
+
+/**
+ * @brief This actor component represents the player's inventory.
+ * The player has several equip slots (left hand, right hand) and a main inventory.
+ */
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class MEATLOAF_API UPlayerInventoryActorComponent : public UActorComponent, public IInventoryActorComponentInterface
+{
+	GENERATED_BODY()
+
+private:
+	/**
+	* @brief This will keep track of the inventory's contents.
+	*/
+	UPROPERTY(EditAnywhere)
+	UBaseItemStackContainer* Inventory;
+	
+public:
+	/**
+	* @brief Populate this in blueprints, it contains a definition of all valid items
+	*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Definitions")
+	UDataTable* ItemDefinitions;
+	
+	// Sets default values for this component's properties
+	UPlayerInventoryActorComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable, Category="Player Inventory Single Slot")
+	FItemStack GetItemStackAtIndex(const int Index) const;
+	
+	UFUNCTION(BlueprintCallable, Category="Player Inventory Single Slot")
+	void SetItemStackAtIndex(const int Index, const FItemStack ItemStack);
+
+	UFUNCTION(BlueprintCallable, Category="Player Inventory Actions")
+	void SwapItemStacks(const int FirstIndex, const int SecondIndex);
+	
+	UFUNCTION(BlueprintCallable, Category="Player Inventory Actions")
+	virtual bool CanInsertItemStack_Implementation(const FItemStack ItemStack) const override;
+
+	UFUNCTION(BlueprintCallable, Category="Player Inventory Actions")
+	virtual FItemStack InsertItemStack_Implementation(const FItemStack ItemStack) override;
+
+	UFUNCTION(BlueprintCallable, Category="Player Inventory Actions")
+	virtual bool CanRemoveItemStack_Implementation(const FItemStack ItemStack) const override;
+
+	UFUNCTION(BlueprintCallable, Category="Player Inventory Actions")
+	virtual FItemStack RemoveItemStack_Implementation(const FItemStack ItemStack) override;
+
+	UFUNCTION(BlueprintCallable, Category="Player Inventory All Slots")
+	virtual TArray<FItemStack> GetAllItemStacks_Implementation() const override;
+
+	UFUNCTION(BlueprintCallable, Category="Player Inventory All Slots")
+	virtual TArray<FItemStack> RemoveAllItemStacks_Implementation() override;
+};
